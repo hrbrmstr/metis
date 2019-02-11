a0512cb01817b36257b68869872803306c855b9c
boB Rudis <bob@rud.is>
2019-02-11 08:12:51 -0500

    think i fixed the RStudio crashing bug

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
+  # rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
   rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
 }
